f3837aed486d7f2818a79c2ca73bdf1251cfa111
Marcelo Ramirez <ramirezyapura@gmail.com>
2022-12-06 00:04:15 -0400

    cambiando with de caja 'ancho'

--- primeraP.js
+++ primeraP.js
@@ -2,10 +2,10 @@ let x = 0
 let y = 0
 let box = document.getElementById("box")
 function move(){
-    if(x < 450 && y == 0){
+    if(x <= 250 && y == 0){
         box.style.left = x + "px"
         x++
-    }else if(x == 450 && y < 350){
+    }else if(x == 251 && y < 350){
         box.style.top = y + "px"
         y++
     }else if(x >= 0 && y == 350){
